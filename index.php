605ee886ebfc23efb69b4bb28112bbf94dc264a7
Joseph Ogbu <fallyfox@gmail.com>
2022-07-06 13:46:58 +0100

    font awesome and bootstrap assets

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav>
+        <ul class="primary-nav">
+            <li class="nav-tab"><a href="#">Home</a></li>
+            <li class="nav-tab"><a href="#">Investor Relations</a></li>
+            <li class="nav-tab"><a href="#">Jobs</a></li>
+            <li class="nav-tab"><a href="#">Specialists</a></li>
+        </ul>
+        <span>Login to MediApp </span>
+    </nav>
+</body>
+</html>
