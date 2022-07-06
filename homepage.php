13658a7612cd68eae0f91cf2cf525f78bfe8c7f1
Winnie Davis <davisbeulah07@gmail.com>
2022-07-06 13:57:36 +0100

    home page sections

---
+++ homepage.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="homepage.css">
+</head>
+<body>
+    <!-- head section -->
+    <section class="container-fluid background ">
+        <div class="container ">
+            <div class="row ps-5" >
+                <div class="col-12 col-sm-12 col-lg-6 col-md-12 content ms-3 text-white">
+                    <h1 class="mb-3">Your health doesn’t have to wait</h1>
+                    <p class="mb-5">Get fast access to diagnosis and treatment with leading specialists</p>
+                    <a href="" class="btn  bg-info text-white  enquiry"> make an enquiry</a>
+                </div>
+                <div class="col-12 col-sm-12 col-lg-6 col-md-12 " >
+                    <img src="images/health2.jpg" class="contentImg" alt="" srcset="">
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <!-- mapping section which includes locations nearby -->
+    <section class="mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-lg-7 col-md-12">
+                    <h1 style="font-weight: lighter; font-size: 50px;">Find your nearest Spire hospital or clinic</h1>
+                    <p>We have 39 hospitals and 8 clinics across the UK</p>
+                    <input class="form-control py-2 border-left-0 border-right-0 u-border--turquoise js-location-finder pac-target-input" style="border:2px solid  rgb(0, 131, 133); border-radius: 50px;" type="search" placeholder="Enter postcode, town or region" name="locationFinder">
+                </div>
+                <div class="col-12 col-sm-12 col-lg-5 col-md-12 p-4" style="background-color: rgb(229, 241, 241);">
+                    <h1 style="font-weight: lighter;">New to private healthcare?</h1>
+                    <p>Read our guide to going private, paying for your own treatment or using health insurance.</p>
+                    <a href="" style="text-align: left;text-decoration: none;color: rgb(0, 131, 133);">Find out more <i></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- feedback -->
+    <section class="mb-5">
+       <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-lg-7 col-md-12">
+                <h1 style="font-weight: lighter;font-size: 50px;">How can we help?</h1>
+                <p>Search or browse by hospital, treatment or consultant to see what Spire can do for you</p>
+            </div>
+            <div class="col-12 col-sm-12 col-lg-5 col-md-12">
+                
+               <div style="border-left: 1px solid gray;" class="p-3 mb-1">
+                    <div class="d-flex justify-content-between">
+                        <h3 style="font-weight: lighter;">Patient feedback</h3>
+                        <i>stars</i>
+                    </div>
+                    <p>Score based on patients’ likelihood to recommend Spire Healthcare</p>
+                </div>
+               <div>
+                <a href="" style="text-align: left;text-decoration: none;color: rgb(0, 131, 133);" class="ps-5">Read more</a>
+     
+               </div>
+            </div>
+        </div>
+       </div>
+    </section>
+    <!-- consultants ,treatment etc -->
+    <section class="mb-5">
+        <div class="container">
+            <div class="row bord">
+                <div class="col-12 col-lg-4 col-md-12 col-sm-12 info">
+                    <div class="card">
+                        <div class="card-body  p-3">
+                            <h2 style="color: rgb(0, 131, 133);"> Consultant</h2>
+                            <p>Access our network of consultants to find the specialist that’s right for you.</p>
+                        </div>
+                        <div class="card-footer">
+                            <img src="images/consultant.PNG" alt="" class="img-fluid" style="height: 9rem;">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-12 col-sm-12 info">
+                    <div class="card ">
+                        <div class="card-body  p-3">
+                            <h2 style="color: rgb(0, 131, 133);">Treatment</h2>
+                            <p>Looking for a specific treatment or procedure?</p>
+                        </div>
+                        <div class="card-footer">
+                            <img src="images/treatment.PNG" alt=""class="img-fluid" style="height: 9rem;">
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-12 col-sm-12 info">
+                    <div class="card ">
+                        <div class="card-body  p-3">
+                            <h2 style="color: rgb(0, 131, 133);">Patient safety</h2>
+                            <p>Find out about the steps we take to ensure your safety in a Spire hospital.</p>
+                        </div>
+                        <div class="card-footer">
+                            <img src="images/safety.PNG" alt="" class="img-fluid" style="height: 9rem;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- added information and newsletter -->
+    <section class="pt-5 my-5 text-center" style="height: 28em;background-color: rgb(229, 241, 241);" >
+        <div class="" >
+            <div id="brandCaptions" class="carousel slide" data-bs-ride="carousel" >
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#brandCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#brandCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>  
+                  <button type="button" data-bs-target="#brandCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>  
+
+          
+                </div>
+                <div class="carousel-inner container">
+                  <div class="carousel-item active">
+                       <div class="row p-5">
+                           <div class="col-12 col-lg-6 col-md-12 col-sm-12" style="text-align: left;">
+                            <h2 class="large" style="font-weight: lighter;">Get fast access to our services with inSpire health insurance</h2>
+                            <p class="mt-3">With inSpire health insurance you'll get fast access to world-class experts at Spire Healthcare, including GPs and physiotherapists.</p>
+                            <p class="mt-3"><a href="#" style="text-decoration: none; color: rgb(0, 131, 133);">Find out more <i class="fa-ml fal fa-long-arrow-right"></i></a></p>
+                           </div>
+                           <div class="col-12 col-lg-6 col-md-12 col-sm-12">
+                            <img src="images/doctor1.jpg" alt="" class="imageT">
+                           </div>
+                       </div>
+                    </div>
+                  
+                <div class="carousel-item ">
+                    <div class="row p-5">
+                        <div class="col-12 col-lg-6 col-md-12 col-sm-12" style="text-align: left;">
+                            <h2 class="large" style="font-weight: lighter;">Spire Healthcare wins top healthcare industry award</h2>
+                            <p class="mt-3">Spire Healthcare has been named as Best Healthcare Provider Partnership with the NHS at the prestigious HSJ Partnership Awards 2021.</p>
+                            <p class="mt-3"><a href="#" style="text-decoration: none;color: rgb(0, 131, 133);">Read more <i class="fa-ml fal fa-long-arrow-right"></i></a></p>
+                        </div>
+                        <div class="col-12 col-lg-6 col-md-12 col-sm-12">
+                         <img src="images/doctor1.jpg" alt="" class="imageT">
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="carousel-item">
+                    <div class="row p-5">
+                        <div class="col-12 col-lg-6 col-md-12 col-sm-12" style="text-align: left;">
+                            <h2 class="large" style="font-weight: lighter;">How to pay for private medical treatment</h2>
+                            <p class="mt-3">Private medical treatment doesn’t have to be complicated. We’ll take a look at the different options in this video.</p>
+                         <p class="mt-3"><a href="#" style="text-decoration: none;color: rgb(0, 131, 133);">Watch vedio <i class="fa-ml fal fa-long-arrow-right"></i></a></p>
+                        </div>
+                        <div class="col-12 col-lg-6 col-md-12 col-sm-12">
+                         <img src="images/doctor1.jpg" alt="" class="imageT">
+                        </div>
+                    </div>
+                </div>
+                  
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#brandCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#brandCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+          
+           
+           
+           
+        </div>
+    </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
