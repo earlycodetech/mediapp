6e161a3fd1ba6415abaf7af46d0795171df9ba43
davidadeniyi06 <davidoladimeji89@gmail.com>
2022-07-06 21:04:40 +0100

    oladimeji login page

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container my-5  ">
+        <div class="card con ">
+            <div class="card-body ">
+                <form>
+                    <div class=" row mx-5 my-5 pt-2 ">
+                        <div class="col-lg-6">
+                            <label>Email address / User name / Mobile number</label>
+                            <input type="email" name="email" class="form-control info" placeholder="Email address/User name/Mobile number">
+                        </div> 
+                        <div class="col-lg-6">
+                            <label >Password</label>
+                            <input type="password" class="form-control info" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="form-check  mx-5 mt-2 ">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            <p>
+                                Use of MySpire is subject to our Terms.Please tick to confirm that you have read and agree to our
+                                <a href="#">Terms</a> and that you have read,understood and agree to the 
+                                <a href="#" >Privacy Notice</a> 
+                            </p>
+                        </label>
+                    </div>
+                    <div class="  d-md-block mt-3 mx-5 pb-3  ">
+                        <button type="submit" class="btn link">Login</button>
+                        <a href="#" class="btn link2">Register</a> <br>
+                        <span>Forgotten your <a href="#">Password</a></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
