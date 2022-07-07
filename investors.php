19a2358d17da23422be27cebaf26e2e8daaaae47
Okolo Favour <favourokolo@gmail.com>
2022-07-07 15:59:46 +0100

    investor

---
+++ investors.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>investors.spirehealthcare</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+ 
+    <nav class="navbar navbar-expand-lg navbar-dark ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html"> <h1> Spire healthcare<small class="text-muted"></small></h1></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- <li class="nav-item"> -->
+                        <a class="nav-link" href="#">Investors</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">who we are</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact us</a>
+                    </li>
+                </ul>
+                <button class="navbar-text text-dark learn-btn">
+                    <a href="#" class="btn text-light">Enquire</a> shareholder helpline : <a href="#" class="text-dark">08077713451</a> </button> 
+                
+            </div>
+        </div>
+      </nav>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/2020_femaleradiographer_newwash_desktop_srgbnew.jpg" class="d-block w-100" alt="investores">
+          <div class="carousel-caption d-none d-md-block"> 
+              <p><h4>Full year results 2021</h4></p><p style="font-size:30 ;">
+                  <h1 class="text">Self-pay driving exceptional private revenue growth; continued support for NHS</h1>
+              </p>
+              <button class="navbar-text text-success">Download full year results 2021</button>
+              
+            <!-- <h5>World class Auto Insurance</h5>
+            <p>You can always trust us for a reliable and quality auto insurance</p> -->
+          <!-- </div> -->
+        </div>
+        <h1 class="text-2"><p>Experts in private healthcare</p></h1>
+        <h4 class="text-3"><p>Spire Healthcare is a leading independent hospital group in the United Kingdom. We deliver high standards of care to our insured, self-pay and NHS patients across 39 hospitals and 8 clinics.</p></h4>
+        
+            <!-- <div class="d-flex justify-content-evenly">
+              <div class="border border-warning rounded px-3 shadow"> -->
+                <div class="templates">
+                  <div class="card-1 shadow p-3 mb-5 bg-white rounded">
+                    
+                    <span class="product-icon"></span>
+                        
+    
+                        <h3>Investors</h3>
+                        <p>
+                          Report and accounts, results and financial information
+                        </p>
+                      </div>
+                  
+                        <div class="card-2 shadow p-3 mb-5 bg-white rounded">
+                    <span class="product-icon"></span>
+                              
+          
+                              <h3>Who we are</h3>
+                              <p>
+                                Learn more about Spire Healthcare Group plc
+                              </p>
+                        </div>
+                              <div class="card-3 shadow p-3 mb-5 bg-white rounded">
+                    <span class="product-icon"></span>
+                        
+    
+                        <h3>News and contacts</h3>
+                        <p>
+                          Regulatory news, updates and disclosures
+                        </p>
+                      </div>
+                </div>
+              </div>
+                
+                
+                    
+                
+      
+</body>
+</html>
